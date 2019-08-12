0966631f160428a9c15c8f3ecb2bfaf89ecf3b93
luchangjiang <20207075@qq.com>
2019-08-12 23:58:09 +0800

    add CompletableFutureDemo

--- ThreadDemo/src/main/java/base/BaseDemo.java
+++ ThreadDemo/src/main/java/base/BaseDemo.java
@@ -9,13 +9,13 @@ package base;
  */
 public abstract class BaseDemo {
 
-    protected AsyncCall asyncCall = new AsyncCall();
-
+    /*protected AsyncCall asyncCall = new AsyncCall();
+     */
     public abstract void callback(long response);
 
     public void call(){
         System.out.println("发起调用");
-        asyncCall.call(this);
+//        asyncCall.call(this);
         System.out.println("调用返回");
     }
 
