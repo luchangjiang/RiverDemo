b30fa54efebf322acc2484963b432f7fd6d6f399
luchangjiang <20207075@qq.com>
2019-10-20 21:28:01 +0800

    add errorDemo

---
+++ ErrorDemo/src/main/java/com/river/ErrorDemo/ErrorDemoApplication.java
@@ -0,0 +1,28 @@
+package com.river.ErrorDemo;
+
+import com.river.ErrorDemo.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: RiverDemo
+ * @description:
+ * @author: River
+ * @create: 2019-10-20 21:17
+ **/
+public class ErrorDemoApplication {
+    public static void main(String args[]){
+
+        List<User> users = new ArrayList<User>(2);
+
+        for(int i=0; i<2; i++){
+            try{
+                users.add(new User(String.valueOf(i))); //will throw NoClassDefFoundError
+                System.out.println("user added");
+            }catch(Throwable t){
+                t.printStackTrace();
+            }
+        }
+    }
+}
